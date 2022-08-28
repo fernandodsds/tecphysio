56c3a8798c7fd83115d6fdb6bbe952a5bde8a326
fernandodsds36 <fernandodsds36@gmail.com>
2022-08-28 15:55:51 -0300

    Box messages in development

--- js/pose.js
+++ js/pose.js
@@ -1,7 +1,7 @@
-const video5 = document.getElementsByClassName('input_video5')[0];
-const out5 = document.getElementsByClassName('output5')[0];
-const controlsElement5 = document.getElementsByClassName('control5')[0];
-const canvasCtx5 = out5.getContext('2d');
+const video = document.getElementsByClassName('input_video')[0];
+const out = document.getElementsByClassName('output5')[0];
+const controlsElement = document.getElementsByClassName('control5')[0];
+const canvasCtx = out.getContext('2d');
 const fpsControl = new FPS();
 const CARD_WIDTH = 150
 const CARD_HEIGHT = 150
@@ -27,12 +27,17 @@ console.log(images)
 var curPerc = 0
 var defaultCard = {"x": 10, "y":10, "image":base_image, "pressed":false, "revealed":false }
 var cards = []
-var sizeWidth = canvasCtx5.canvas.clientWidth;
-var sizeHeight = canvasCtx5.canvas.clientHeight;
+var sizeWidth = out.offsetWidth;
+var sizeHeight = out.offsetHeight;
 var startTime=new Date();
 var cursorPos = {x:0, y:0};
 var gameInitialized = false;
+var gameReadyGo = false
 var revealTime = 0;
+var gameScore = 0 
+var qtdeJogos = 0
+
+canvasCtx.font = "20px Arial";
 
 function shuffleArray(array) {
  
@@ -40,23 +45,95 @@ function shuffleArray(array) {
 
 }
 
-images = shuffleArray(images)
-console.log(images)
-for (var i = 0; i<3;i++){
-  console.log(15 + (CARD_WIDTH*(i)))
-  cards.push({"x": 15*(i) + (CARD_WIDTH*(i)), "y":10, "image":images[i], 'revealed_image_id':images[i],"pressed":false, "revealed":false, "startTime":new Date(), "elapsed":0 });
+function initGame()
+{
+  cards = []
+  images = shuffleArray(images)
+  console.log(images)
+  
+  for (var i = 0; i<3;i++){
+    console.log(15 + (CARD_WIDTH*(i)))
+    cards.push({"x": 15*(i) + (CARD_WIDTH*(i)), "y":10, "image":images[i], 'revealed_image_id':images[i],"pressed":false, "revealed":false, "startTime":new Date(), "elapsed":0 });
+  }
+  
+  for (var i = 3; i<6;i++){
+    cards.push({"x": 15*(i-3) + (CARD_WIDTH*(i-3)), "y":CARD_HEIGHT+40, "image":images[i], 'revealed_image_id':images[i],"pressed":false, "revealed":false, "startTime":new Date(), "elapsed":0 });
+  }
+  return cards
 }
 
-for (var i = 3; i<6;i++){
-  cards.push({"x": 15*(i-3) + (CARD_WIDTH*(i-3)), "y":CARD_HEIGHT+40, "image":images[i], 'revealed_image_id':images[i],"pressed":false, "revealed":false, "startTime":new Date(), "elapsed":0 });
-}
 
-console.log(cards)
 
 function zColor(data) {
   const z = clamp(data.from.z + 0.5, 0, 1);
   return `rgba(0, ${255 * z}, ${255 * (1 - z)}, 1)`;
 }
+
+function onButtonCursor(buttonPos, cursorPos){
+  //console.log(cardPos)
+  //console.log(cursorPos)
+  if (cursorPos.x >= buttonPos.x && cursorPos.x <= (buttonPos.x + buttonPos.width) && cursorPos.y >= buttonPos.y && cursorPos.y <= (buttonPos.y + buttonPos.height)) {
+    return true
+  } else if(cursorPos.x >= (buttonPos.x + buttonPos.width) && cursorPos.x <= buttonPos.x && cursorPos.y  >= (buttonPos.y + buttonPos.height) && cursorPos.y  <= buttonPos.y){
+    return true
+  }
+  
+  return false
+
+}
+
+
+function drawMessage(ctx, text){
+  boxWidth = 200
+  boxHeight = 100
+  ctx.fillStyle = 'white';
+  ctx.rect((out.width/2)-(boxWidth/2), (out.height*0.4 -(boxHeight/2)) , boxWidth, boxHeight);
+  ctx.fill();
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.font = "12px Arial";
+
+  textWidth = ctx.measureText(text ).width;
+  ctx.fillStyle = 'black';
+  ctx.fillText(text , (out.width/2) - (textWidth / 2), (out.height*0.4))  
+  console.log(textWidth)
+  ctx.fill();
+  ctx.stroke();
+}
+
+function drawConfirmMessage(ctx){
+  drawMessage(ctx,"Teste");
+  btnWidth = 50
+  btnHeight = 20
+  btn1 = {
+    "x":(out.width/2)-(btnWidth*1.5),
+    "y": (out.height*0.46 -(btnHeight/2)),
+    "width":btnWidth,
+    "height": btnHeight
+  }
+  
+  btn2 = {
+    "x":(out.width/2)-(btnWidth*-0.6),
+    "y": (out.height*0.46 -(btnHeight/2)),
+    "width":btnWidth,
+    "height": btnHeight
+  }  
+  //onButtonCursor
+
+  ctx.rect( btn1.x,  btn1.y, btn1.width,  btn1.height);
+
+  ctx.rect( btn2.x,  btn2.y, btn2.width,  btn2.height);
+
+  if (onButtonCursor(btn1, cursorPos))
+  {
+    ctx.fillText('Colision', cursorPos.x, cursorPos.y)    
+  };
+
+  if (onButtonCursor(btn2, cursorPos)){
+    ctx.fillText('Colision', cursorPos.x, cursorPos.y)    
+  };
+}
+
 
 function onCardCursor(cardPos, cursorPos){
   //console.log(cardPos)
@@ -80,46 +157,33 @@ function drawImageCard(image,context, x,y)
   context.drawImage(image, x, y, CARD_WIDTH, CARD_HEIGHT);
 }
 
-function onResultsPose(results) {
-  TotalElapsedTime=parseInt((new Date() - startTime)/1000);
+function validateEndedGame(toValidationCards){
+  return toValidationCards.every( (val, i, arr) => val.revealed === arr[0].revealed )
+}
 
-  document.body.classList.add('loaded');
-  fpsControl.tick();
+cards = initGame()
 
-  canvasCtx5.save();
-  canvasCtx5.globalAlpha = 0.25;
-  canvasCtx5.clearRect(0, 0, out5.width, out5.height);
-  canvasCtx5.drawImage(
-    results.image, 0, 0, out5.width, out5.height); 
-  
-  canvasCtx5.restore();    
-  canvasCtx5.stroke();
-
+function memoryGame(){
+    
   for (var i = 0; i <6 ;i++){
-    drawImageCard( cards[i].image,canvasCtx5,cards[i].x,cards[i].y);  
+    drawImageCard( cards[i].image,canvasCtx,cards[i].x,cards[i].y);  
   }  
-  //console.log(cards)
-  drawLandmarks(
-      canvasCtx5,
-      Object.values([[19]])
-          .map(index => results.poseLandmarks[index]),
-      {color: zColor, fillColor: '#FF0000'});
-  canvasCtx5.font = "30px Arial";
-  cursorPos.x = results.poseLandmarks[19].x*out5.width;
-  cursorPos.y = results.poseLandmarks[19].y*out5.height;
+    
   if (TotalElapsedTime <=5){
+    //verifyIsEqualsCard(selectedCards)
     console.log(TotalElapsedTime)
     return
   }
 
   if (gameInitialized == false){
+    //cards = initGame()
     cards.forEach(element => {
       element.image = base_image
     });
     gameInitialized = true;
   }
 
-
+  
   for (var i = 0; i<6;i++){
     if (onCardCursor(cards[i], cursorPos) 
         && cards[i].elapsed <= 3 
@@ -140,7 +204,7 @@ function onResultsPose(results) {
         }  
 
       }
-      canvasCtx5.fillText(cards[i].elapsed, cursorPos.x, cursorPos.y)    
+      canvasCtx.fillText(cards[i].elapsed, cursorPos.x, cursorPos.y)    
       //console.log(cards[i].elapsed)
 
     } else {
@@ -149,7 +213,6 @@ function onResultsPose(results) {
     }
 
     if (selectedCards.length == 2 && (TotalElapsedTime - revealTime) > 2 ){ 
-      console.log(verifyIsEqualsCard(selectedCards))
       if (!verifyIsEqualsCard(selectedCards) ) 
       {
         cards[selectedCards[0]].image = base_image;
@@ -159,13 +222,55 @@ function onResultsPose(results) {
         cards[selectedCards[0]].elapsed = 0;
         cards[selectedCards[1]].elapsed = 0;
       }
-      
+      else 
+      {
+        gameScore += 1
+        document.getElementById("score").innerHTML = ""+gameScore;
+      }
+      if(validateEndedGame(cards) ){
+        qtdeJogos += 1
+        document.getElementById("qtdeJogos").innerHTML = ""+qtdeJogos;
+      }
       selectedCards = []
     }
- 
+
     
 
   }
+
+}
+
+function onResultsPose(results) {
+  TotalElapsedTime=parseInt((new Date() - startTime)/1000);
+
+  document.body.classList.add('loaded');
+  fpsControl.tick();
+
+  canvasCtx.save();
+  canvasCtx.globalAlpha = 0.25;
+  canvasCtx.clearRect(0, 0, out.width, out.height);
+  canvasCtx.drawImage(
+    results.image, 0, 0, out.width, out.height); 
+  
+  canvasCtx.restore();    
+  canvasCtx.stroke();
+
+  if (gameReadyGo == false){
+    drawConfirmMessage(canvasCtx);
+  }else{
+    memoryGame();
+  }
+  
+  drawLandmarks(
+    canvasCtx,
+    Object.values([[19]])
+        .map(index => results.poseLandmarks[index]),
+    {color: zColor, fillColor: '#FF0000'});
+
+cursorPos.x = results.poseLandmarks[19].x*out.width;
+cursorPos.y = results.poseLandmarks[19].y*out.height;
+  
+
 
 }
 
@@ -174,16 +279,16 @@ const pose = new Pose({locateFile: (file) => {
 }});
 pose.onResults(onResultsPose);
 
-const camera = new Camera(video5, {
+const camera = new Camera(video, {
   onFrame: async () => {
-    await pose.send({image: video5});
+    await pose.send({image: video});
   },
   width: 480,
   height: 480
 });
 camera.start();
 
-new ControlPanel(controlsElement5, {
+new ControlPanel(controlsElement, {
       selfieMode: true,
       upperBodyOnly: false,
       smoothLandmarks: true,
@@ -210,6 +315,6 @@ new ControlPanel(controlsElement5, {
       }),
     ])
     .on(options => {
-      video5.classList.toggle('selfie', options.selfieMode);
+      video.classList.toggle('selfie', options.selfieMode);
       pose.setOptions(options);
     });
